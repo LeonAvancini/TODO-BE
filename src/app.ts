0752e8aa967414dff06466dc9a7e87607589e568
leonavancini <avancinileon@gmail.com>
2024-03-02 10:59:38 +0100

    Initial configuration

---
+++ src/app.ts
@@ -0,0 +1,11 @@
+import express from 'express';
+const app = express();
+const port = 3000;
+
+app.listen(port, () => {
+  return console.log(`Express is listening at http://localhost:${port}`);
+});
+
+app.get('/', (req, res) => {
+    res.send('Hello Worldsito!');
+  });
